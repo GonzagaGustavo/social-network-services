b15ad4b96a8d1e9a6e213fc89aa858491fb9d2ea
Gustavo Gonzaga <Gustavogonzaga.gg243@gmail.com>
2023-12-20 16:14:44 -0300

    Update consumers.ts

--- search/src/consumers.ts
+++ search/src/consumers.ts
@@ -1,13 +1,22 @@
-import { Call } from "@grpc/grpc-js";
+import { ServerUnaryCall } from "@grpc/grpc-js";
 import logger from "./logger";
 
-export async function get(call: any, callback: any) {
+interface MyRequest {}
+interface MyResponse {
+  id: string;
+  user_id: number;
+  search: string;
+}
+
+export async function get(
+  call: ServerUnaryCall<MyRequest, MyResponse>,
+  callback: any
+) {
   callback(null, {
     id: "213",
     user_id: 1,
     search: "thought that I",
   });
-
   // call.on("data", (data) => {
   //   logger.info("data", data);
   // });
